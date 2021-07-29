8a7a44dfca952cb951c026f0462a8ad16a76f173
Archer Scherer <ascherer831@gmail.com>
2021-07-29 14:08:03 -0500

    validations buit, need to add to views

--- jobSearch_app/models.py
+++ jobSearch_app/models.py
@@ -3,15 +3,115 @@ from login_reg_app.models import User
 
 class JobManager(models.Manager):
 
-    def position_create_manager(self, post_data):
+    def position_create_manager(self, postData):
         errors = {}
-        if len(post_data['position']) < 5:
+        if len(postData['position']) < 5:
             errors['position'] = "Please make your position more than four characters"
         return errors
+
+class FormManager(models.manager):
+
+    def create_job_validator(self, postData):
+
+    def create_note_validator(self, postData):
+        errors = {}
+    
+        if len(postData['desc']) < 1:
+            errors['empty'] = "Note cannot be blank"
+        if len(postData['desc']) < 255:
+            errors['note'] = "Note cannot be longer than 255 characters"
+        return errors
+
+
+    def create_interview_helper_validator(self, postData):
+        if len(postData['elevator-pitch']) < 1:
+            errors['elevator_empty'] = "Elevator Pitch cannot be blank"
+        if len(postData['elevator-pitch']) < 255:
+            errors['elevator_long'] = "Elevator Pitch cannot be longer than 255 characters"
+
+        if len(postData['str_weak']) < 1:
+            errors['str_weak_empty'] = "Strengths and Weaknesses cannot be blank"
+        if len(postData['str_weak']) < 255:
+            errors['str_weak_long'] = "Strengths and Weaknesses cannot be longer than 255 characters"
+
+        if len(postData['accomplishments']) < 1:
+            errors['accomplishments_empty'] = "Accomplishments cannot be blank"
+        if len(postData['accomplishments']) < 255:
+            errors['accomplishments_long'] = "Accomplishments cannot be longer than 255 characters"
+        
+        if len(postData['common_qa']) < 1:
+            errors['common_qa_empty'] = "Common Q&A cannot be blank"
+        if len(postData['common_qa']) < 255:
+            errors['common_qa_long'] = "Common Q&A cannot be longer than 255 characters"
+        return errors
+
+    def create_job_interest_validator(self, postData):
+        if len(postData['title']) < 1:
+            errors['interest_empty'] = "cannot be blank"
+        if len(postData['title']) < 50:
+            errors['interest_long'] = "cannot be longer than 50 characters"
+        return errors
+
+    def create_loc_interest_validator(self, postData):
+        if len(postData['city']) < 1:
+            errors['city_empty'] = "cannot be blank"
+        if len(postData['city']) < 50:
+            errors['city_long'] = "cannot be longer than 50 characters"
+        if len(postData['state']) < 2:
+            errors['state_empty'] = "Must be 2 characters"
+        if len(postData['state']) <2:
+            errors['state_long'] = "Must be 2 characters"
+        return errors
+
+    def create_job_validator(self, postData):
+        if len(postData['company']) < 1:
+            errors['company_empty'] = "cannot be blank"
+        if len(postData['company']) < 50:
+            errors['company_long'] = "cannot be longer than 50 characters" 
+
+        if len(postData['job_title']) < 1:
+            errors['job_title_empty'] = "cannot be blank"
+        if len(postData['job_title']) < 50:
+            errors['job_title_long'] = "cannot be longer than 50 characters" 
+
+        if len(postData['city']) < 1:
+            errors['city_empty'] = "cannot be blank"
+        if len(postData['city']) < 50:
+            errors['city_long'] = "cannot be longer than 50 characters"
+        if len(postData['state']) < 2:
+            errors['state_empty'] = "Must be 2 characters"
+        if len(postData['state']) <2:
+            errors['state_long'] = "Must be 2 characters"
+
+        if len(postData['post_date']) < 1:
+            errors['post_date_empty'] = "cannot be blank"
+
+
+        if len(postData['job_url']) < 1:
+            errors['job_url_empty'] = "cannot be blank"
+
+        if len(postData['min']) < 1:
+            errors['min_empty'] = "cannot be blank"
+            
+        if len(postData['max']) < 1:
+            errors['max_empty'] = "cannot be blank"
+
+        if len(postData['summary']) < 1:
+            errors['summary_empty'] = "cannot be blank"
+        if len(postData['summary']) < 255:
+            errors['summary_long'] = "cannot be longer than 255 characters"
+
+        if len(postData['description']) < 1:
+            errors['description_empty'] = "cannot be blank"
+
+
+
+
 
 
 class State(models.Model):
     abbr = models.CharField(max_length=2)
+    objects = FormManager()
 
     def __str__(self):
         return self.abbr
@@ -22,7 +122,7 @@ class Location(models.Model):
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
     loc_saves = models.ManyToManyField(User, related_name="user_loc_saves")
-
+    objects = FormManager()
 
     def __str__(self):
         return f"{self.city}, {self.state.abbr}"
@@ -32,7 +132,7 @@ class Position(models.Model):
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
     pos_saves = models.ManyToManyField(User, related_name="user_pos_saves")
-
+    objects = FormManager()
     def __str__(self):
         return f"{self.title}"
 
@@ -40,6 +140,7 @@ class Qualification(models.Model):
     name = models.CharField(max_length=45)
     duration = models.IntegerField(default=None, null=True)
     required = models.BooleanField(default=False)
+    objects = FormManager()
 
     def __str__(self):
         return f"{self.name}"
@@ -59,6 +160,7 @@ class Job(models.Model):
     dislikes = models.ManyToManyField(User, related_name="job_dislikes")
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
+    objects = FormManager()
 
     def __str__(self):
         return f"{self.job_title}, {self.company}"
@@ -69,6 +171,7 @@ class Note(models.Model):
     desc = models.TextField()
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
+    objects = FormManager()
 
     def __str__(self):
         return f"Note by: {self.creator.first_name} {self.creator.last_name} for Job: {self.job_id}"
@@ -79,29 +182,34 @@ class ElevatorPitch(models.Model):
     elevator_pitch = models.TextField()
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
+    objects = FormManager()
 
 class Strength_Weakness(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     str_weak = models.TextField()
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
+    objects = FormManager()
 
 class Accomplishments(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     accomplishments = models.TextField()
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
+    objects = FormManager()
     
 class CommonQA(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     common_qa = models.TextField()
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
+    objects = FormManager()
     
 class General(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     general = models.TextField()
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
+    objects = FormManager()
 
 
